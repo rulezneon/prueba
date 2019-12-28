18bf0d2c8f0742859cd4561cf039c407cbfd9e01
t-hernjes <jesus.hc@globant.com>
2019-12-28 14:40:59 -0600

    Objetos

---
+++ Dom/Television.js
@@ -0,0 +1,28 @@
+
+
+{
+
+    Pantalla={
+        Alto= 80,
+        Ancho= 100
+    },
+    Conexiones=["HDMI","VGA", "USB", "Infrarojo"],
+    Botones=
+    [
+        Encender={
+            Simbolo="On/Off",
+            EjecutarAccion=fnPrenderApagar,
+        },
+        Encender={
+            Simbolo="On/Off",
+            EjecutarAccion=fnPrenderApagar,
+        },
+    ]
+
+}
+
+
+
+
+
+
